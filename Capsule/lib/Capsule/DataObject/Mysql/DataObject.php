78c33cd9c35247a5b2b0cfb47c83825096bb15ac
polyanin <polyanin@gmail.com>
2015-11-06 00:52:05 +0500

    data object

--- Capsule/lib/Capsule/DataObject/Mysql/DataObject.php
+++ Capsule/lib/Capsule/DataObject/Mysql/DataObject.php
@@ -9,6 +9,10 @@ namespace Capsule\DataObject\Mysql;
 
 use Capsule\Capsule;
 use Capsule\Common\Path;
+use Capsule\DataObject\Inflector;
+use Capsule\Db\Db;
+use Capsule\Exception;
+use Capsule\Tools\Tools;
 
 abstract class DataObject
 {
@@ -72,10 +76,11 @@ abstract class DataObject
     final protected static function _reflectionClass()
     {
         $class = get_called_class();
-        if (!isset(self::$common[$class][__FUNCTION__])) {
-            self::$common[$class][__FUNCTION__] = new \ReflectionClass($class);
+        $f = __FUNCTION__;
+        if (!isset(self::$common[$class][$f])) {
+            self::$common[$class][$f] = new \ReflectionClass($class);
         }
-        return self::$common[$class][__FUNCTION__];
+        return self::$common[$class][$f];
     }
 
     /**
@@ -87,15 +92,94 @@ abstract class DataObject
     public static function _configLocation()
     {
         $class = get_called_class();
-        if (!isset(self::$common[$class][__FUNCTION__])) {
-            self::$common[$class][__FUNCTION__] =
+        $f = __FUNCTION__;
+        if (!isset(self::$common[$class][$f])) {
+            self::$common[$class][$f] =
                 new Path(Capsule::getInstance()->{Capsule::DIR_CFG}, $class);
         }
-        return self::$common[$class][__FUNCTION__];
+        return self::$common[$class][$f];
     }
 
+    /**
+     * Returns automatically generated associated table name by class name
+     *
+     * @param void
+     * @return string
+     */
     public static function _associatedTable()
     {
-        
+        $class = get_called_class();
+        $f = __FUNCTION__;
+        if (!isset(self::$common[$class][$f])) {
+            self::$common[$class][$f] = Inflector::getInstance()->getAssociatedTable($class);
+        }
+        return self::$common[$class][$f];
+    }
+
+    /**
+     * Создает config file
+     *
+     * @param string $filename
+     * @return Path
+     * @throws Exception
+     */
+    protected static function _createFileConfig($filename)
+    {
+        $dir = self::_configLocation();
+        $path = new Path($dir, $filename);
+        if (file_exists($path)) {
+            return $path;
+        }
+        if (!is_dir($dir)) {
+            $is_dir = mkdir($dir, 0700, true);
+            if (!$is_dir || !is_dir($dir)) {
+                $msg = 'Unable to create config location directory: ' . $dir;
+                throw new Exception($msg);
+            }
+        }
+        file_put_contents($path, '');
+        if (file_exists($path)) {
+            return $path;
+        }
+        throw new Exception('Unable to create configuration file: ' . $path);
+    }
+
+    public static function _createFileConfigDefault()
+    {
+        return self::_createFileConfig(self::FILENAME_CONFIG_DEFAULT);
+    }
+
+    public static function _createFileConfigUser()
+    {
+        return self::_createFileConfig(self::FILENAME_CONFIG_USER);
+    }
+
+    public static function _createFileSqlCreateTable()
+    {
+        return self::_createFileConfig(self::FILENAME_SQL_CREATE_TABLE);
+    }
+
+    public static function _createFileSqlInitScript()
+    {
+        return self::_createFileConfig(self::FILENAME_SQL_INIT_SCRIPT);
+    }
+
+    public static function _createConfigFiles()
+    {
+        return array(
+            self::FILENAME_CONFIG_DEFAULT => self::_createFileConfigDefault(),
+            self::FILENAME_CONFIG_USER => self::_createFileConfigUser(),
+            self::FILENAME_SQL_CREATE_TABLE => self::_createFileSqlCreateTable(),
+            self::FILENAME_SQL_INIT_SCRIPT => self::_createFileSqlInitScript()
+        );
+    }
+
+    public static function _loadInformationSchema()
+    {
+        $db = Db::getInstance();
+        $default_schema = $db->config->dbname;
+        $current_schema = $db->selectSchema();
+        if ($de)
+            Tools::dump($default_schema);
     }
 }
